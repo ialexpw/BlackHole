6984ec47704541869a47b3ad4b4188f9346d9e7a
Alex White <alex@picotory.com>
2020-09-16 14:36:59 +0100

    Correct url + closing div

--- index.php
+++ index.php
@@ -48,7 +48,8 @@
 
 		// Button
 		$urlTpl .= '<div class="col-md-1">';
-		$urlTpl .= '<a class="btn btn-info" href="index.php?rq=' . $genToken . '&clear" role="button">Clear</a>';
+		$urlTpl .= '<a class="btn btn-info" href="index.php?rq=' . $_GET['rq'] . '&clear" role="button">Clear</a>';
+		$urlTpl .= '</div>';
 		$urlTpl .= '</div>';
 
 		// Are we inspecting the logs
