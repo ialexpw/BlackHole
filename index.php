4ef118b8e051db14fe76bd73957f4fb97f5b9388
Alex White <alex@picotory.com>
2020-09-18 14:26:04 +0100

    hsc

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 				if(file_exists($_GET['details'] . '.txt')) {
 					// Viewing the raw details
 					if(isset($_GET['raw'])) {
-						echo file_get_contents($_GET['details'] . '.txt');
+						echo htmlspecialchars(file_get_contents($_GET['details'] . '.txt'));
 						exit();
 					}
 				}
